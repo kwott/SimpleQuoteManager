f5bb00145d2fcec38f8d0271c97da9da191afc47
Pierre Rudloff <contact@rudloff.pro>
2015-01-15 22:33:01 +0100

    Add constructor to Mdl_MCB_Client_Data and Mdl_MCB_Data

--- application/modules_core/mcb_data/models/mdl_mcb_client_data.php
+++ application/modules_core/mcb_data/models/mdl_mcb_client_data.php
@@ -3,90 +3,94 @@
 class Mdl_MCB_Client_Data extends MY_Model {
 
     public $settings;
+    
+    public function __construct() {
+        $this->settings = new stdClass();
+    }
 
-	public function get($client_id, $key) {
+    public function get($client_id, $key) {
 
-		$this->db->select('mcb_client_value');
+        $this->db->select('mcb_client_value');
 
-		$this->db->where('mcb_client_key', $key);
+        $this->db->where('mcb_client_key', $key);
 
-		$this->db->where('client_id', $client_id);
+        $this->db->where('client_id', $client_id);
 
-		$query = $this->db->get('mcb_client_data');
+        $query = $this->db->get('mcb_client_data');
 
-		if ($query->row()) {
+        if ($query->row()) {
 
-			return $query->row()->mcb_client_value;
+            return $query->row()->mcb_client_value;
 
-		}
+        }
 
-		else {
+        else {
 
-			return NULL;
+            return NULL;
 
-		}
+        }
 
-	}
+    }
 
-	public function save($client_id, $key, $value) {
+    public function save($client_id, $key, $value) {
 
-		if (!is_null($this->get($client_id, $key))) {
+        if (!is_null($this->get($client_id, $key))) {
 
-			$this->db->where('mcb_client_key', $key);
+            $this->db->where('mcb_client_key', $key);
 
-			$this->db->where('client_id', $client_id);
+            $this->db->where('client_id', $client_id);
 
-			$db_array = array(
-				'mcb_client_value'	=>	$value
-			);
+            $db_array = array(
+                'mcb_client_value'  =>  $value
+            );
 
-			$this->db->update('mcb_client_data', $db_array);
+            $this->db->update('mcb_client_data', $db_array);
 
-		}
+        }
 
-		else {
+        else {
 
-			$db_array = array(
-				'client_id'			=>	$client_id,
-				'mcb_client_key'	=>	$key,
-				'mcb_client_value'	=>	$value
-			);
+            $db_array = array(
+                'client_id'         =>  $client_id,
+                'mcb_client_key'    =>  $key,
+                'mcb_client_value'  =>  $value
+            );
 
-			$this->db->insert('mcb_client_data', $db_array);
+            $this->db->insert('mcb_client_data', $db_array);
 
-		}
+        }
 
-	}
+    }
 
-	public function delete($client_id, $key) {
+    public function delete($client_id, $key) {
 
-		$this->db->where('mcb_client_key', $key);
+        $this->db->where('mcb_client_key', $key);
 
-		$this->db->where('client_id', $client_id);
+        $this->db->where('client_id', $client_id);
 
-		$this->db->delete('mcb_client_data');
+        $this->db->delete('mcb_client_data');
 
-	}
+    }
 
-	public function set_session_data($client_id) {
+    public function set_session_data($client_id) {
 
-		$this->db->where('client_id', $client_id);
+        $this->db->where('client_id', $client_id);
 
-		$mcb_client_data = $this->db->get('mcb_client_data')->result();
+        $mcb_client_data = $this->db->get('mcb_client_data')->result();
 
-		foreach ($mcb_client_data as $data) {
+        foreach ($mcb_client_data as $data) {
 
-			$this->settings->{$data->mcb_client_key} = $data->mcb_client_value;
+            $this->settings->{$data->mcb_client_key} = $data->mcb_client_value;
 
-		}
+        }
 
-	}
+    }
 
-	public function setting($key) {
+    public function setting($key) {
 
-		return (isset($this->settings->$key)) ? $this->settings->$key : NULL;
+        return (isset($this->settings->$key)) ? $this->settings->$key : NULL;
 
-	}
+    }
 
 }
 
--- application/modules_core/mcb_data/models/mdl_mcb_userdata.php
+++ application/modules_core/mcb_data/models/mdl_mcb_userdata.php
@@ -2,104 +2,108 @@
 
 class Mdl_MCB_Userdata extends MY_Model {
 
-	public $settings;
+    public $settings;
+    
+    public function __construct() {
+        $this->settings = new stdClass();
+    }
 
-	public function get($user_id, $key) {
+    public function get($user_id, $key) {
 
-		$this->db->select('mcb_userdata_value');
+        $this->db->select('mcb_userdata_value');
 
-		$this->db->where('mcb_userdata_user_id', $user_id);
-		$this->db->where('mcb_userdata_key', $key);
+        $this->db->where('mcb_userdata_user_id', $user_id);
+        $this->db->where('mcb_userdata_key', $key);
 
-		$query = $this->db->get('mcb_userdata');
+        $query = $this->db->get('mcb_userdata');
 
-		if ($query->row()) {
+        if ($query->row()) {
 
-			return $query->row()->mcb_userdata_value;
+            return $query->row()->mcb_userdata_value;
 
-		}
+        }
 
-		else {
+        else {
 
-			return NULL;
+            return NULL;
 
-		}
+        }
 
-	}
+    }
 
-	public function save($user_id, $key, $value) {
+    public function save($user_id, $key, $value) {
 
-		if (!is_null($this->get($user_id, $key))) {
+        if (!is_null($this->get($user_id, $key))) {
 
-			$this->db->where('mcb_userdata_user_id', $user_id);
-			$this->db->where('mcb_userdata_key', $key);
+            $this->db->where('mcb_userdata_user_id', $user_id);
+            $this->db->where('mcb_userdata_key', $key);
 
-			$db_array = array(
-				'mcb_userdata_value'	=>	$value
-			);
+            $db_array = array(
+                'mcb_userdata_value'    =>  $value
+            );
 
-			$this->db->update('mcb_userdata', $db_array);
+            $this->db->update('mcb_userdata', $db_array);
 
-		}
+        }
 
-		else {
+        else {
 
-			$db_array = array(
-				'mcb_userdata_user_id'	=>	$user_id,
-				'mcb_userdata_key'		=>	$key,
-				'mcb_userdata_value'	=>	$value
-			);
+            $db_array = array(
+                'mcb_userdata_user_id'  =>  $user_id,
+                'mcb_userdata_key'      =>  $key,
+                'mcb_userdata_value'    =>  $value
+            );
 
-			$this->db->insert('mcb_userdata', $db_array);
+            $this->db->insert('mcb_userdata', $db_array);
 
-		}
+        }
 
-	}
+    }
 
-	public function delete($user_id, $key) {
+    public function delete($user_id, $key) {
 
-		$this->db->where('mcb_userdata_user_id', $user_id);
-		$this->db->where('mcb_userdata_key', $key);
+        $this->db->where('mcb_userdata_user_id', $user_id);
+        $this->db->where('mcb_userdata_key', $key);
 
-		$this->db->delete('mcb_userdata');
+        $this->db->delete('mcb_userdata');
 
-	}
+    }
 
-	public function set_session_data($user_id) {
+    public function set_session_data($user_id) {
 
-		$this->db->where('mcb_userdata_user_id', $user_id);
+        $this->db->where('mcb_userdata_user_id', $user_id);
 
-		$mcb_userdata = $this->db->get('mcb_userdata')->result();
+        $mcb_userdata = $this->db->get('mcb_userdata')->result();
 
-		foreach ($mcb_userdata as $data) {
+        foreach ($mcb_userdata as $data) {
 
-			$this->settings->{$data->mcb_userdata_key} = $data->mcb_userdata_value;
+            $this->settings->{$data->mcb_userdata_key} = $data->mcb_userdata_value;
 
-		}
+        }
 
-	}
+    }
 
-	public function setting($key) {
+    public function setting($key) {
 
-		return (isset($this->settings->$key)) ? $this->settings->$key : NULL;
+        return (isset($this->settings->$key)) ? $this->settings->$key : NULL;
 
-	}
+    }
 
-	public function set_setting($key, $value) {
+    public function set_setting($key, $value) {
 
-		$this->settings->$key = $value;
+        $this->settings->$key = $value;
 
-	}
+    }
 
-	public function save_settings($user_id, $user_settings) {
+    public function save_settings($user_id, $user_settings) {
 
-		foreach ($user_settings as $key=>$value) {
+        foreach ($user_settings as $key=>$value) {
 
-			$this->mdl_mcb_userdata->save($user_id, $key, $value);
+            $this->mdl_mcb_userdata->save($user_id, $key, $value);
 
-		}
+        }
 
-	}
+    }
 
 }
 
