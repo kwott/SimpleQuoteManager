edf4fcf2f95152f496638a8afea232beeff1fb73
Pierre Rudloff <contact@rudloff.pro>
2015-01-18 03:59:47 +0100

    PHPCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,33 @@
 /*jslint node: true */
 module.exports = function (grunt) {
     'use strict';
-    grunt.initConfig({
-        cssmin: {
-            combine: {
-                files: {
-                    'assets/style/dist/main.css': ['assets/style/css/reset.css', 'assets/style/css/layout.css', 'assets/style/css/general.css', 'assets/style/css/grid.css', 'assets/style/css/messages.css', 'assets/style/css/pagination.css']
+    grunt.initConfig(
+        {
+            cssmin: {
+                combine: {
+                    files: {
+                        'assets/style/dist/main.css': ['assets/style/css/reset.css', 'assets/style/css/layout.css', 'assets/style/css/general.css', 'assets/style/css/grid.css', 'assets/style/css/messages.css', 'assets/style/css/pagination.css']
+                    }
+                }
+            },
+            watch: {
+                styles: {
+                    files: ['assets/style/css/*.css'],
+                    tasks: ['cssmin']
+                }
+            },
+            jslint: {
+                Gruntfile: {
+                    src: ['Gruntfile.js']
+                }
+            },
+            csslint: {
+                css: {
+                    src: ['assets/style/css/*.css']
                 }
             }
-        },
-        watch: {
-            styles: {
-                files: ['assets/style/css/*.css'],
-                tasks: ['cssmin']
-            }
-        },
-        jslint: {
-            Gruntfile: {
-                src: ['Gruntfile.js']
-            }
-        },
-        csslint: {
-            css: {
-                src: ['assets/style/css/*.css']
-            }
         }
-    });
+    );
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
