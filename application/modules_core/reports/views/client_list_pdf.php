63108a10653b43ea32082bfbd4510408690714c7
Pierre Rudloff <contact@rudloff.pro>
2015-01-18 15:53:33 +0100

    Use HTML5 on every page (fixes #27) Indentation

--- application/modules_core/reports/views/client_list_pdf.php
+++ application/modules_core/reports/views/client_list_pdf.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-		<?php $this->load->view('css'); ?>
+        <?php $this->load->view('css'); ?>
 
-	</head>
-	<body>
+    </head>
+    <body>
 
-		<h1><?php echo $this->lang->line('client_list'); ?></h1>
+        <h1><?php echo $this->lang->line('client_list'); ?></h1>
 
-		<?php $this->load->view('client_list_view'); ?>
+        <?php $this->load->view('client_list_view'); ?>
 
-	</body>
+    </body>
 </html>
--- application/modules_core/reports/views/inventory_history_pdf.php
+++ application/modules_core/reports/views/inventory_history_pdf.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-		<?php $this->load->view('css'); ?>
+        <?php $this->load->view('css'); ?>
 
-	</head>
-	<body>
-		
-		<h1><?php echo $this->lang->line('inventory_history'); ?></h1>
+    </head>
+    <body>
+        
+        <h1><?php echo $this->lang->line('inventory_history'); ?></h1>
 
-		<?php $this->load->view('inventory_history_view'); ?>
+        <?php $this->load->view('inventory_history_view'); ?>
 
-	</body>
+    </body>
 </html>
