8f70842c720d00a414329eab7983cf0ddb3af9d5
Pierre Rudloff <contact@rudloff.pro>
2015-03-04 17:02:42 +0100

    Add sass task for kwott theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,10 @@ module.exports = function (grunt) {
                 mcb: {
                     files: ['themes/mcb/css/*.css', '!themes/mcb/css/main.css'],
                     tasks: ['cssmin']
+                },
+                kwott: {
+                    files: ['themes/kwott/scss/*.scss'],
+                    tasks: ['sass']
                 }
             },
             jslint: {
@@ -25,6 +29,16 @@ module.exports = function (grunt) {
                 mcb: {
                     src: ['themes/mcb/css/*.css', '!themes/mcb/css/main.css']
                 }
+            },
+            sass: {
+                kwott: {
+                    options: {
+                        style: 'compressed'
+                    },
+                    files: {
+                        'themes/kwott/css/main.css': 'themes/kwott/scss/main.scss'
+                    }
+                }
             }
         }
     );
@@ -33,7 +47,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin', 'sass']);
     grunt.registerTask('lint', ['jslint', 'csslint']);
 };
